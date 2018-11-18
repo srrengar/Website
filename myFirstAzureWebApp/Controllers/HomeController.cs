e292e68610c15f35834e2a581de41c85538cc4e8
Sravan Rengarajan <srrengar@microsoft.com>
2018-11-18 15:40:27 -0500

    Add project files.

---
+++ myFirstAzureWebApp/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace myFirstAzureWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("newindex");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Sravan's application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Sravan's contact page.";
+
+            return View();
+        }
+    }
+}
